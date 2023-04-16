c0efbc52be42f1a82efc3d4eb7814ddad34f2501
Xohat <xohatlatte@gmail.com>
2023-04-16 22:22:37 +0200

    Cleaning

--- PracticaV2/mesh-loader/documentation/html/search/classes_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['view_0',['View',['../classrender__engine_1_1_view.html',1,'render_engine']]]
-];
--- PracticaV2/mesh-loader/documentation/html/search/files_5.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['view_2ecpp_0',['View.cpp',['../_view_8cpp.html',1,'']]],
-  ['view_2ehpp_1',['View.hpp',['../_view_8hpp.html',1,'']]]
-];
